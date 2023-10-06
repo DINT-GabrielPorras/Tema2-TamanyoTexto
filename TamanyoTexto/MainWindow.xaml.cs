34e4af85b44f4eaed8ed375616984a548f8ec594
Gabriel Porras <gabrielporrasperez@gmail.com>
2023-10-06 17:59:20 +0200

    Agregar archivos de proyecto.

---
+++ TamanyoTexto/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TamanyoTexto
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void botonPequeño_Checked(object sender, RoutedEventArgs e)
+        {
+            textoTitulo.FontSize = 36;
+
+        }
+
+        private void botonMediano_Checked(object sender, RoutedEventArgs e)
+        {
+            textoTitulo.FontSize = 48;
+        }
+
+        private void botonGrande_Checked(object sender, RoutedEventArgs e)
+        {
+            textoTitulo.FontSize = 72;
+        }
+    }
+}
